b29abcc741dccb1b56b7b757570d5bd7ab9976f2
s18827 <s18827@pjwstk.edu.pl>
2020-11-18 20:21:07 +0100

    05: Added completed 5a.js

---
+++ 05/5a.js
@@ -0,0 +1,35 @@
+var person = {
+    name: 'Bob',
+    surname: 'Ross',
+    birthdate: 1968,
+
+    calcAge : function() {
+        return new Date().getFullYear() - this.birthdate;
+    },
+
+    introduce : function() {
+        return "Hi, my name is " + this.name + " " + this.surname
+         +", I'm " + this.calcAge() + " years old.";
+    }
+    
+}
+
+console.log(person.introduce()+"\n");
+
+function printProps(obj) {
+    props = [];
+    propTypes = [];
+
+    Object.keys(obj).forEach((prop) => props.push(prop));
+
+    for(const prop in obj) {
+        propTypes.push(typeof obj[prop]);
+    }
+    
+    for(let i = 0; i < props.length; i++) {
+        console.log(props[i] + ": " + propTypes[i]);
+    }
+}
+
+// console.log(person);
+printProps(person);
