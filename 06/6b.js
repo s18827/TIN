65b47e00296a378730b1cfeeb9265b036448226d
s18827 <s18827@pjwstk.edu.pl>
2020-11-25 20:40:35 +0100

    6b: Added completed task

---
+++ 06/6b.js
@@ -0,0 +1,38 @@
+(function () {
+
+    const wait = ms => {
+        let start = new Date().getTime();
+        let end = start;
+        while(end < start + ms) {
+          end = new Date().getTime();
+        }
+    }
+    
+    const addPara = function() {
+        wait(2000);
+        var p = "Quis imperdiet massa tincidunt nunc pulvinar sapien et ligula. Sagittis purus sit amet volutpat consequat mauris nunc congue. Donec et odio pellentesque diam volutpat commodo. Sed libero enim sed faucibus. Aenean sed adipiscing diam donec adipiscing tristique risus. Odio morbi quis commodo odio aenean. Quam quisque id diam vel. Nullam non nisi est sit amet facilisis magna etiam. Donec ac odio tempor orci dapibus ultrices in iaculis. Ultrices in iaculis nunc sed augue lacus. Posuere ac ut consequat semper viverra nam libero.";
+
+        document.getElementById("para-add").innerHTML = p;
+    }
+
+    addPara();
+
+})();
+
+// function wait(ms){
+//     var start = new Date().getTime();
+//     var end = start;
+//     while(end < start + ms) {
+//       end = new Date().getTime();
+//    }
+//    return;
+//  }
+
+// const addPara = function() {
+//     wait(5000);
+//     let p = "Quis imperdiet massa tincidunt nunc pulvinar sapien et ligula. Sagittis purus sit amet volutpat consequat mauris nunc congue. Donec et odio pellentesque diam volutpat commodo. Sed libero enim sed faucibus. Aenean sed adipiscing diam donec adipiscing tristique risus. Odio morbi quis commodo odio aenean. Quam quisque id diam vel. Nullam non nisi est sit amet facilisis magna etiam. Donec ac odio tempor orci dapibus ultrices in iaculis. Ultrices in iaculis nunc sed augue lacus. Posuere ac ut consequat semper viverra nam libero.";
+
+//     document.getElementById("para-add").innerHTML = p;
+// }
+
+// document.addEventListener('DOMContentLoaded', addPara);
