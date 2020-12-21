1a6302a05f5f7c5eabcc6f9fd9d8c1ba4f3cc4ec
s18827 <s18827@pjwstk.edu.pl>
2020-12-21 23:06:23 +0100

    7a.js: Completed task

--- 07/7a.js
+++ 07/7a.js
@@ -2,40 +2,88 @@
 // 'use strict;'
 const http = require('http');
 const url = require('url');
-// const StringDecoder = require("string_decoder").StringDecoder;
-// const util = require('util');
-// const formidable = require('formidable');
+const util = require('util');
 
-let lol = 9;
-function fun() {
-    
+const server = http.createServer(function (req, res) {
+  let path = url.parse(req.url, true);
+  let query = util.inspect(path.query);
+  let operands = getParamsFromQueryStr(query, res);
+  let result = resolveOperation(path.pathname, operands, res);
+
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end(
+    operands[0] +
+      ' ' +
+      result[0] +
+      ' ' +
+      operands[1] +
+      ' = ' +
+      result[1] +
+      '\n\n'
+  );
+});
+
+const port = 8080;
+
+server.listen(port, function () {
+  console.log('Listening on port ' + port);
+});
+
+function getParamsFromQueryStr(queryStr, res) {
+  let operands = [];
+  let vars = queryStr.toLowerCase().split("'");
+  for (let i = 0; i < vars.length; i++) {
+    if (/^\d+$/.test(vars[i]) || /^\d+\.\d+$/.test(vars[i])) {
+      // checking for numeric values only
+      operands.push(parseInt(vars[i]));
+    }
+  }
+  if (operands.length < 2) {
+    error(res, 400, 'Enter 2 operands!');
+  } else return operands;
 }
-const server = http.createServer(function(req, res) {
-    
-    // req.method.toLowerCase() // etc get post
-    // console.log('req.url: ' + req.url);
-    let path = url.parse(req.url, true);
-    // console.log('path pathname: ' + path.pathname);
 
+function resolveOperation(strPathName, operands, res) {
+  let oper = strPathName.toLowerCase();
+  let x = operands[0];
+  let y = operands[1];
 
-    // res.setHeader('Content-type','application/json');
-    // res.setHeader('Access-Control-Allow-Origin',"*");
-    // res.writeHead(200); // status code
+  switch (oper) {
+    case '/add':
+      return ['+', add(x, y)];
+    case '/sub':
+      return ['-', sub(x, y)];
+    case '/mul':
+      return ['*', mul(x, y)];
+    case '/div':
+      return ['/', div(x, y, res)];
+    default:
+      error(
+        res,
+        404,
+        'Incorrect operation signature (choose one from: add/sub/mul/div)'
+      );
+  }
+}
 
-    // let dataObj = {"id":123, "name": "Bob", "email": "bob@email.com"};
-    // let data = JSON.stringify(dataObj);
-    // res.end(data);
-    // console.log(req.headers);
+const add = function (x, y) {
+  return x + y;
+};
 
-    res.setHeader("200", "OK", {'Content-Type': 'text/plain'});
-    res.write("The response\n\n");
-    res.write("path.pathname: " + path.pathname);
-    res.end("\nEnd of message to browser");
+const sub = function (x, y) {
+  return x - y;
+};
 
-});
+const mul = function (x, y) {
+  return x * y;
+};
 
-const port = 1234;
+const div = function (x, y, res) {
+  if (y == 0) error(res, 400, "You can't divide by 0!");
+  return x / y;
+};
 
-server.listen(port, function() {
-    console.log('Listening on port ' + port);
-});
+function error(res, code, message) {
+  res.writeHead(code, { 'Content-Type': 'text/plain' });
+  res.end('Error: ' + message);
+}
